01aa3dd4cffea1f062796c32bbcf2f8066b75003
Ramin K <ramin@snappytv.com>
2011-09-14 13:17:47 -0700

    default in the wrong stanza

--- yum/manifests/atrpms.pp
+++ yum/manifests/atrpms.pp
@@ -2,26 +2,26 @@ class yum::atrpms {
 
   case $::operatingsystem {
     Fedora: {
-        yumrepo { 'atrpms':
-          baseurl  => 'http://dl.atrpms.net/f$releasever-$basearch/atrpms/stable',
-          descr    => 'Fedora Core $releasever - $basearch - ATrpms',
-          enabled  => 1,
-          gpgcheck => 1,
-          gpgkey   => 'http://ATrpms.net/RPM-GPG-KEY.atrpms',
-          exclude  => 'fxload xine* nagios* nrpe*',
-        }
+      yumrepo { 'atrpms':
+        baseurl  => 'http://dl.atrpms.net/f$releasever-$basearch/atrpms/stable',
+        descr    => 'Fedora Core $releasever - $basearch - ATrpms',
+        enabled  => 1,
+        gpgcheck => 1,
+        gpgkey   => 'http://ATrpms.net/RPM-GPG-KEY.atrpms',
+        exclude  => 'fxload xine* nagios* nrpe*',
+      }
     }
     CentOS: {
-        yumrepo { 'atrpms':
-          baseurl  => 'http://dl.atrpms.net/el$releasever-$basearch/atrpms/stable',
-          descr    => 'CentOS-$releasever - ATrpms',
-          enabled  => 1,
-          gpgcheck => 1,
-          gpgkey   => 'http://ATrpms.net/RPM-GPG-KEY.atrpms',
-          exclude  => 'fxload xine* nagios* nrpe*',
-        }
-    default: {}
+      yumrepo { 'atrpms':
+        baseurl  => 'http://dl.atrpms.net/el$releasever-$basearch/atrpms/stable',
+        descr    => 'CentOS-$releasever - ATrpms',
+        enabled  => 1,
+        gpgcheck => 1,
+        gpgkey   => 'http://ATrpms.net/RPM-GPG-KEY.atrpms',
+        exclude  => 'fxload xine* nagios* nrpe*',
+      }
     }
+    default: {}
   }
 
 }
