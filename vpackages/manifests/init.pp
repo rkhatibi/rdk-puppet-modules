8d73b3f66c1d544c97b2b08db768fc11ee9592a5
Ramin K <ramin@snappytv.com>
2011-09-19 14:19:30 -0700

    trying out this vpackage idea

---
+++ vpackages/manifests/init.pp
@@ -0,0 +1,11 @@
+class vpackages {
+
+  package { 'htop': }
+  package { 'innotop': }
+  package { 'maatkit': }
+  package { 'mysql-client': }
+  package { 'screen': }
+  package { 'tcpdump': }
+  package { 'whois': }
+    
+}
