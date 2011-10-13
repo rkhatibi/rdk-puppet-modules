e08afa3339705a2f96c7b49a5d80564474f4ed63
Ramin K <ramin@snappytv.com>
2011-10-13 06:41:49 -0700

    quoting and README cleanup

--- apt/manifests/install.pp
+++ apt/manifests/install.pp
@@ -1,7 +1,7 @@
 class apt::install {
 
-  package { "apt": ensure => present, }
-  package { "apt-utils": ensure => present, }
-  package { "aptitude": ensure => present, }
+  package { 'apt': ensure => present, }
+  package { 'apt-utils': ensure => present, }
+  package { 'aptitude': ensure => present, }
 
 }
