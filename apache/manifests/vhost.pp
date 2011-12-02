79e6857aace1fcd854ac01e74661658105f0b4af
Ramin K <ramin@snappytv.com>
2011-12-02 15:32:56 -0800

    add default priority

--- apache/manifests/vhost.pp
+++ apache/manifests/vhost.pp
@@ -1,4 +1,4 @@
-define apache::vhost {
+define apache::vhost ($priority=99) {
 
 # $name is a reserved variable, it works based on the name of this define
 # apache::vhost { "this_becomes_name": }
@@ -7,13 +7,13 @@ define apache::vhost {
 
   file { "apache/vhost.d/${name}":
     ensure  => present,
-    path    => "${apache::params::vhostdir}/${name}",
-    mode    => $apache::params::configfile_mode,
-    owner   => $apache::params::configfile_owner,
-    group   => $apache::params::configfile_group,
+    path    => "${apache::params::vhostdir}/${priority}_${name}",
+    mode    => "${apache::params::configfile_mode}",
+    owner   => "${apache::params::configfile_owner}",
+    group   => "${apache::params::configfile_group}",
     notify  => Class['apache::service'],
     require => Class['apache::config'],
-    content => template("apache/vhosts/${name}.erb"),
+    content => template("apache/vhosts/$name.erb"),
   }
 
 }
